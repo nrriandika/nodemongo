e2a4a9ab77e16577f18708517e89d936f78e89c3
Novandi Resya Riandika <nr.riandika@gmail.com>
2022-07-07 16:41:26 +0700

    nodejs mongodb

---
+++ routes/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { check, validationResult } = require("express-validator");
+const path = require("path");
+const auth = require("http-auth");
+const router = express.Router();
+const Registration = mongoose.model("Registration");
+const basic = auth.basic({
+  file: path.join(__dirname, "../users.htpasswd"),
+});
+
+//get registration form
+router.get("/", (req, res) => {
+  res.render("form", { title: "Registration form" });
+});
+
+//save data to mongodb
+router.post(
+  "/",
+  [
+    check("name").isLength({ min: 1 }).withMessage("Please enter a name"),
+    check("email").isLength({ min: 1 }).withMessage("Please enter an email"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      const registration = new Registration(req.body);
+      registration
+        .save()
+        .then(() => {
+          res.send("Thank you for your registration!");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send("Sorry! Something went wrong.");
+        });
+    } else {
+    }
+  }
+);
+
+//Get data from mongodb
+router.get("/registrations", (req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render("index", { title: "Listing registrations", registrations });
+    })
+    .catch(() => {
+      res.send("Sorry! Something went wrong.");
+    });
+});
+
+module.exports = router;
+
+router.get(
+  "/registrations",
+  basic.check((req, res) => {})
+);
